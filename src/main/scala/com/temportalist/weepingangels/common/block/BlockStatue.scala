84cea136a6d715f8387cfd8f1de6e374a6dcdd1e
The Temportalist <the.country.gamer@gmail.com>
2015-08-19 15:49:27 -0400

    Fixed missing plnith texture and duplicate statue drops, addresses  issues #21 & #22

--- src/main/scala/com/temportalist/weepingangels/common/block/BlockStatue.scala
+++ src/main/scala/com/temportalist/weepingangels/common/block/BlockStatue.scala
@@ -1,7 +1,9 @@
 package com.temportalist.weepingangels.common.block
 
+import java.util
+
 import com.temportalist.origin.api.common.block.BlockTile
-import com.temportalist.origin.api.common.lib.V3O
+import com.temportalist.origin.api.common.lib.{BlockState, V3O}
 import com.temportalist.origin.api.common.utility.Stacks
 import com.temportalist.weepingangels.common.tile.TEStatue
 import com.temportalist.weepingangels.common.{WAOptions, WeepingAngels}
@@ -55,14 +57,17 @@ class BlockStatue(material: Material, name: String, teclass: Class[_ <: TileEnti
 	}
 
 	override def breakBlock(world: World, x: Int, y: Int, z: Int, block: Block, meta: Int): Unit = {
-		for (y1 <- 1 to meta) world.setBlockToAir(x, y - y1, z)
-		for (y1 <- 1 until this.maxHeight - meta) world.setBlockToAir(x, y + y1, z)
 		if (meta == 0) {
+			for (y1 <- 1 to meta) world.setBlockToAir(x, y - y1, z)
+			for (y1 <- 1 until this.maxHeight - meta) world.setBlockToAir(x, y + y1, z)
 			world.removeTileEntity(x, y, z)
-			if (this.isServer()) Stacks.spawnItemStack(
-				world, new V3O(x, y, z) + V3O.CENTER, new ItemStack(this), world.rand, 10
-			)
 		}
+	}
+
+	override def getDrops_Pre(world: World, pos: V3O, state: BlockState,
+			tile: TileEntity): util.List[ItemStack] = {
+		if (state.getMeta == 0) super.getDrops_Pre(world, pos, state, tile)
+		else new util.ArrayList[ItemStack]()
 	}
 
 	override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, placer: EntityLivingBase,
